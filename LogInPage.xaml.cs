a38ebff690ea426127fe7096bd1d7a9fa9a36b35
gyang6633 <gyang6633@gmail.com>
2024-09-17 16:36:42 -0500

    added login and signup pages init

---
+++ LogInPage.xaml.cs
@@ -0,0 +1,8 @@
+namespace MauiApp3;
+public partial class LogInPage : ContentPage
+{
+    public LogInPage()
+    {
+        InitializeComponent();  // This calls the generated code from the XAML file
+    }
+}
---
+++ SignInPage.xaml.cs
@@ -0,0 +1,8 @@
+namespace MauiApp3;
+public partial class SignInPage : ContentPage
+{
+	public SignInPage()
+	{
+		InitializeComponent();  // This calls the generated code from the XAML file
+	}
+}
